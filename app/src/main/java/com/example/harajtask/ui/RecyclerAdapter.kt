66193177c8e51091d4ab9825d9a52048eb0bee62
Alejandro <ariel10aguero@gmail.com>
2021-05-24 13:09:49 -0300

    gradle update

---
+++ app/src/main/java/com/example/harajtask/ui/RecyclerAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.harajtask.ui
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.harajtask.databinding.ItemRowBinding
+
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class MainViewHolder(val binding: ItemRowBinding) :RecyclerView.ViewHolder(binding.root)
+}
