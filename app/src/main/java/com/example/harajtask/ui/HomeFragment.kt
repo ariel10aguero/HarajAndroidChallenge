f3291b40cd4ae3d2658d79ba24f4380b93095633
Alejandro <ariel10aguero@gmail.com>
2021-05-25 15:30:37 -0300

    search bar clickeable

--- app/src/main/java/com/example/harajtask/ui/HomeFragment.kt
+++ app/src/main/java/com/example/harajtask/ui/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
