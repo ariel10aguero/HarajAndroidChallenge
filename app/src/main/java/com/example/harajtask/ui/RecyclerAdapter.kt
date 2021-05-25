564408968f22a88440d5fd78d2e7c34498af92f0
Alejandro <ariel10aguero@gmail.com>
2021-05-24 21:07:43 -0300

    building adapter

--- app/src/main/java/com/example/harajtask/ui/RecyclerAdapter.kt
+++ app/src/main/java/com/example/harajtask/ui/RecyclerAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.harajtask.ui
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.harajtask.data.Post
 import com.example.harajtask.databinding.ItemRowBinding
 
@@ -22,11 +24,30 @@ class RecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val currentItem = postList[position]
+        when(holder){
+            is MainViewHolder -> holder.bind(currentItem)
+        }
+
     }
 
     override fun getItemCount(): Int {
-        return 60
+        return postList.size
     }
 
-    inner class MainViewHolder(val binding: ItemRowBinding) :RecyclerView.ViewHolder(binding.root)
+    inner class MainViewHolder(val binding: ItemRowBinding) :RecyclerView.ViewHolder(binding.root){
+
+        fun bind(post: Post){
+            binding.apply {
+                Glide.with(itemView.context.applicationContext).load(post.thumbURL).into(imageView)
+                titleTxt.text = post.title
+                cityTxt.text = post.city
+                dateTxt.text = post.date.toString()
+                commentsNum.text = post.commentCount.toString()
+                usernameTxt.text = post.username
+            }
+
+        }
+
+    }
 }
